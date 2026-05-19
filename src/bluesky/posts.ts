e84ea1cc4d477a5c34daeddb6ddc144988c4124c
kei <105542355+nearfriend@users.noreply.github.com>
2026-05-19 19:24:08 +0900

    Update posts.ts

--- src/bluesky/posts.ts
+++ src/bluesky/posts.ts
@@ -2,5 +2,8 @@ import { agent } from './client'
 
 export async function publishNewPost(text: string) {
   console.log('Publishing new post', { text })
-  await agent.post({ text })
+  await agent.post({
+    $type: 'app.bsky.feed.post',
+    text: text
+  })
 }
