a69653f36d85f893ca34460390cf36bc62507c94
kei <105542355+nearfriend@users.noreply.github.com>
2026-05-19 19:24:57 +0900

    catch error

--- src/bluesky/posts.ts
+++ src/bluesky/posts.ts
@@ -2,8 +2,13 @@ import { agent } from './client'
 
 export async function publishNewPost(text: string) {
   console.log('Publishing new post', { text })
-  await agent.post({
-    $type: 'app.bsky.feed.post',
-    text: text
-  })
+  try {
+    await agent.post({
+      $type: 'app.bsky.feed.post',
+      text: text
+    })
+  } catch (error) {
+    console.log('Error publishing post', { error })
+    throw error
+  }
 }
